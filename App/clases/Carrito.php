8f448d8065e91651ad623eae14bc62bfd30cb982
EmilioMM <emarmor346@g.educaand.es>
2024-11-26 12:47:10 +0100

    Creación del metodo eliminar producto

--- App/clases/Carrito.php
+++ App/clases/Carrito.php
@@ -1,10 +1,26 @@
 <?php
+
 namespace app\clases;
 
-class Carrito {
+class Carrito
+{
     private array $productos = [];
 
-    public function agregarProducto(Producto $producto): void {
+    public function agregarProducto(Producto $producto): void
+    {
         $this->productos[] = $producto;
+    }
+
+    public function eliminarProducto(string $id): void
+    {
+        foreach ($this->productos as $indice => $producto) {
+            if ($producto->id == $id) {
+                $posicion = $indice;
+            }
+        }
+
+        if (!empty($posicion)) {
+            unset($this->productos[$posicion]);
+        }
     }
 }
