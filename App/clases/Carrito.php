fabaf8d35b3156072fcc643319f848ff6a84f473
MiguelVaroRaya <mvarray@g.educaand.es>
2024-11-26 18:09:57 +0100

    Modifico index.php y Carrito.php, añadiendo el construct y modificando agregar producto

--- App/clases/Carrito.php
+++ App/clases/Carrito.php
@@ -4,11 +4,18 @@ namespace app\clases;
 
 class Carrito
 {
-    private array $productos = [];
+    private array $productos;
+
+    public function __construct()
+    {
+        $this->productos = $_SESSION["productos"];
+    }
 
     public function agregarProducto(Producto $producto): void
     {
         $this->productos[] = $producto;
+
+        $_SESSION["productos"] = $this->productos;
     }
 
     public function eliminarProducto(string $id): void
@@ -33,7 +40,7 @@ class Carrito
 
         return $total;
     }
-
+    
     public function vaciarCarrito(): void {
         $this->productos = [];
     }
--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+session_start();
+
 require_once '../autoload.php';
 require_once '../routes/web.php';
 
