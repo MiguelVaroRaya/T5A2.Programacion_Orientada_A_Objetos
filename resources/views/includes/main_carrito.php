2f811bead3e55cfade85badc55aabda8ad2590e0
Miguel Varo <migvarray@g.educaand.com>
2024-12-01 00:14:23 +0100

    Añado la funcionalidad de vaciar el carrito

--- resources/views/includes/main_carrito.php
+++ resources/views/includes/main_carrito.php
@@ -8,5 +8,24 @@ $carrito = new Carrito();
 
 <main class="main">
     <?php $carrito->mostrarCarrito() ?>
-    
+
+    <form class="formulario" action="/carrito" method="post">
+        <input type="submit" name="borrar" value="Vaciar Carrito"></input>
+    </form>
 </main>
+
+<?php
+
+function filtrado(string $datos): string
+{
+    $datos = trim($datos); // Elimina espacios antes y después de los datos 
+    $datos = stripslashes($datos); // Elimina backslashes \ 
+    $datos = htmlspecialchars($datos);  // Traduce caracteres especiales en entidades HTML 
+    return $datos;
+}
+
+if (isset($_POST["borrar"]) && $_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $carrito->vaciarCarrito();
+    header('Location: /carrito');
+}
